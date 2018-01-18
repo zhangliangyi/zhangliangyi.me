b6692958d79156386cf6ffced1dd0570384550f1
zhangliangyi <zhang.liangyi@outlook.com>
2018-01-19 00:37:31 +0800

    Adjust the layout of hobby section

--- src/components/Hobby/Hobby.js
+++ src/components/Hobby/Hobby.js
@@ -23,12 +23,12 @@ export default class Hobby extends Component {
     return (
       <div className="text-center py-4">
         <SectionHeader icon="heart" header="Hobby"/>
-        <div className="row">
+        <div className="row mt-4">
           {
             this.props.hobbies.map((hobby, index) => (
-              <div className="col p-md-4" key={index}>
+              <div className="col px-md-4" key={index}>
                 <img className="rounded-circle hobby-image" src={this.state.images[hobby.name]}/>
-                <p className="text-bold text-secondary">{hobby.text}</p>
+                <p className="text-bold text-secondary my-2">{hobby.text}</p>
               </div>
             ))
           }
